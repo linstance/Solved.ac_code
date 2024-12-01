5e26c9b735202f137c279fac27ee02b95586c7bf
Jawon_LEE <allen030916@gmail.com>
2024-12-01 17:09:08 +0900

    cat

---
+++ The_seeds/No.10171_고양이/No.10171_고양이/No.10171_고양이.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "\\    ";
+    cout << "/\\\n";
+    cout << " )  ";
+    cout << "( ')\n";
+    cout << "(  ";
+    cout << "/  )\n";
+    cout << " \\(__)|";
+    
+    return 0;
+}
