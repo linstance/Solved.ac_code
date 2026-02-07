6218d6e2f14e9c4c53b519f3acd51a539110cf62
Jawon_LEE <66247624+linstance@users.noreply.github.com>
2026-02-07 20:51:52 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout<< "         ,r\'\"7" <<std::endl;
+    std::cout<< "r`-_   ,'  ,/" << std::endl;
+    std::cout<< " \\. \". L_r'" << std::endl;
+    std::cout<< "   `~\\/" << std::endl;
+    std::cout<< "      |" << std::endl;
+    std::cout<< "      |" << std::endl;
+    return 0;
+}
